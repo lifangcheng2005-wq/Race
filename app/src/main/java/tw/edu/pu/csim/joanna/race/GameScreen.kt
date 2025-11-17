fcc5b4c30455feaad6aa863b0aad9f719acaa073
joanna <lifangcheng2005@gmail.com>
2025-11-17 15:21:41 +0800

    v6.載入馬匹

--- app/src/main/java/tw/edu/pu/csim/joanna/race/GameScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/joanna/race/GameScreen.kt
@@ -13,11 +13,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun GameScreen(message: String,gameViewModel: GameViewModel) {
+    //載入圖片
+    val imageBitmap = ImageBitmap.imageResource(R.drawable.horse0)
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -39,6 +46,12 @@ fun GameScreen(message: String,gameViewModel: GameViewModel) {
                 radius = 100f,
                 center = Offset(gameViewModel.circleX, gameViewModel.circleY)
             )
+            drawImage(
+                image = imageBitmap,
+                dstOffset = IntOffset(0, 100),
+                dstSize = IntSize(200, 200)
+            )
+
         }
 
         Text(
